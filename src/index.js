67f6163227a9a941e143ce355533a154048f1e9a
GabrielaDiazB <gabriela.dbravo@gmail.com>
2019-04-22 06:57:03 -0500

    correcciones de estilos

--- src/index.js
+++ src/index.js
@@ -12,62 +12,37 @@ $(function() {
   
 });
 
-let slideIndexOne = 0;
-showSlidesOne();
+let slideIndex = 1;
+showSlides(slideIndex);
+let num = [];
 
-function showSlidesOne() {
-  let i;
-  let slidesOne = document.getElementsByClassName("mySlides1");
-  let dotsOne = document.getElementsByClassName("dot1");
-  for (i = 0; i < slidesOne.length; i++) {
-    slidesOne[i].style.display = "none";  
-  }
-  slideIndexOne++;
-  if (slideIndexOne > slidesOne.length) {slideIndexOne = 1}    
-  for (i = 0; i < dotsOne.length; i++) {
-    dotsOne[i].className = dotsOne[i].className.replace(" active", "");
-  }
-  slidesOne[slideIndexOne-1].style.display = "block";  
-  dotsOne[slideIndexOne-1].className += " active";
-  setTimeout(showSlidesOne, 2000); // Change image every 2 seconds
+function plusSlides(num) {
+  showSlides(slideIndex += num);
 }
+plusSlides(num);
 
-let slideIndexTwo = 0;
-showSlidesTwo();
-
-function showSlidesTwo() {
-  let i;
-  let slidesTwo = document.getElementsByClassName("mySlides2");
-  let dotsTwo = document.getElementsByClassName("dot2");
-  for (i = 0; i < slidesTwo.length; i++) {
-    slidesTwo[i].style.display = "none";  
-  }
-  slideIndexTwo++;
-  if (slideIndexTwo > slidesTwo.length) {slideIndexTwo = 1}    
-  for (i = 0; i < dotsTwo.length; i++) {
-    dotsTwo[i].className = dotsTwo[i].className.replace(" active", "");
-  }
-  slidesTwo[slideIndexTwo-1].style.display = "block";  
-  dotsTwo[slideIndexTwo-1].className += " active";
-  setTimeout(showSlidesTwo, 3000); // Change image every 2 seconds
+function currentSlide(num) {
+  showSlides(slideIndex = num);
 }
+currentSlide(num);
 
-let slideIndexThree = 0;
-showSlidesThree();
-
-function showSlidesThree() {
+function showSlides(num) {
   let i;
-  let slidesThree = document.getElementsByClassName("mySlides3");
-  let dotsThree = document.getElementsByClassName("dot3");
-  for (i = 0; i < slidesThree.length; i++) {
-    slidesThree[i].style.display = "none";  
+  const slides = document.getElementsByClassName('mySlides');
+  const dots = document.getElementsByClassName('dot');
+  if (num > slides.length) {
+    slideIndex = 1;
+  }    
+  if (num < 1) {
+    slideIndex = slides.length;
   }
-  slideIndexThree++;
-  if (slideIndexThree > slidesThree.length) {slideIndexThree = 1}    
-  for (i = 0; i < dotsThree.length; i++) {
-    dotsThree[i].className = dotsThree[i].className.replace(" active", "");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';  
   }
-  slidesThree[slideIndexThree-1].style.display = "block";  
-  dotsThree[slideIndexThree-1].className += " active";
-  setTimeout(showSlidesThree, 2500); // Change image every 2 seconds
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active-slide-img', '');
+  }
+
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active-slide-img';  
 }
